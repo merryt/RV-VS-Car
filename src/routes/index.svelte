fabbfa758917845fa45a5067b427a229e0ad1213
merryt <tymerry@gmail.com>
2022-07-07 17:09:51 -0600

    setting up for rv slider

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
+<script>
+  let rvMPG = 15;
+  let rvAverageCostPerNight = 0;
+  let rvPurchasePrice = 20000;
+
+  let carMPG = 15;
+  let carAverageCostPerNight = 0;
+  let carPurchasePrice = 20000;
+
+  let averageMilesTraveledPerDay = 10;
+  let totalGoalMiles = 3000;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
